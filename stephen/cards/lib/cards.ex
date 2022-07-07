bb29c95092f70dab3a743712f1a8fb1cd274cb5f
Demétrius Mânica <demeteixeira@hotmail.com>
2022-07-07 18:36:54 -0300

    Added @moduledoc and @doc for some functions

--- stephen/cards/lib/cards.ex
+++ stephen/cards/lib/cards.ex
@@ -1,4 +1,16 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of playing cards
+  """
+
+  @doc """
+    Returns a list of strings representing a deck of playing cards.
+
+  ## Example
+
+      iex> create_deck()
+      ["Ace of Spades", "Ace of Hearts", ...]
+  """
   def create_deck do
     values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
@@ -16,6 +28,18 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should
+    be in the hand.
+
+  ## Examples
+
+      iex> deck = Cards.create_deck
+      iex> {hand, deck} = Cards.deal(deck, 1)
+      iex> hand
+      ["Ace of Spades"]
+  """
   def deal(deck, hand_size) do
     deck |> Enum.split(hand_size)
   end
