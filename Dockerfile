32889d78e6fe455cdcc23646fb4050e322f55b31
Demétrius Mânica <demeteixeira@hotmail.com>
2022-07-07 17:51:44 -0300

    Added udemy stephen's initial cards course

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM elixir:1.12.3
+
+# install debian packages
+ENV DEBIAN_FRONTEND=noninteractive
+RUN apt-get update && \
+  apt-get install -y postgresql-client inotify-tools && \
+  apt-get clean
+
+# prepare work dir
+WORKDIR /app
+
+# install phoenix packages
+# RUN mix local.hex --force
+# RUN mix local.rebar --force
+# RUN mix archive.install hex phx_new 1.5.6
+
+# install nodejs
+# RUN curl -sL https://deb.nodesource.com/setup_14.x | bash - && \
+#   apt-get install -y nodejs
+
+# copy files
+# COPY . /app
+
+# compile application
+# RUN mix deps.get
+# RUN mix deps.compile
+# RUN mix compile
+
+# CMD ["mix", "phx.server"]
---
+++ stephen/cards/lib/cards.ex
@@ -0,0 +1,39 @@
+defmodule Cards do
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    deck |> Enum.split(hand_size)
+  end
+
+  def deal_poker_table(hand_size) do
+    deck = create_deck() |> shuffle
+    {sb, deck} = deal(deck, hand_size)
+    {bb, deck} = deal(deck, hand_size)
+    {utg, deck} = deal(deck, hand_size)
+    {utg2, deck} = deal(deck, hand_size)
+    {utg3, deck} = deal(deck, hand_size)
+    {hijack, deck} = deal(deck, hand_size)
+    {cutoff, deck} = deal(deck, hand_size)
+    {button, deck} = deal(deck, hand_size)
+
+    {flop, deck} = deal(deck, 3)
+    {turn, deck} = deal(deck, 1)
+    {river, _deck} = deal(deck, 1)
+    {[sb, bb, utg, utg2, utg3, hijack, cutoff, button], [flop, turn, river]}
+  end
+end
