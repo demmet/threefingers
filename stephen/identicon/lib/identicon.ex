afd1d5146f9d3209584971861d6e7e9bb3848559
Demétrius Mânica <demeteixeira@hotmail.com>
2022-07-08 12:17:03 -0300

    Added initial build_grid function

--- stephen/identicon/lib/identicon.ex
+++ stephen/identicon/lib/identicon.ex
@@ -7,6 +7,16 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
   end
 
   def hash_input(input) do
@@ -16,9 +26,5 @@ defmodule Identicon do
       |> :binary.bin_to_list
 
     %Identicon.Image{hex: hex}
-  end
-
-  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
-    %Identicon.Image{image | color: {r, g, b}}
   end
 end
