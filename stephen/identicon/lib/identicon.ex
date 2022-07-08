0512d294e9cbe8f418753dd133a325e7dfbbed49
Demétrius Mânica <demeteixeira@hotmail.com>
2022-07-08 13:35:33 -0300

    Changed deprecated function

--- stephen/identicon/lib/identicon.ex
+++ stephen/identicon/lib/identicon.ex
@@ -30,7 +30,7 @@ defmodule Identicon do
   def build_grid(%Identicon.Image{hex: hex} = image) do
     grid =
       hex
-      |> Enum.chunk(3)
+      |> Enum.chunk_every(3)
       |> Enum.map(&mirror_row/1)
       |> List.flatten
       |> Enum.with_index
