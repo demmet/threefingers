86f60dc8fbb1a32fc967b384929cd9e59f19384d
Demétrius Mânica <demeteixeira@hotmail.com>
2022-07-09 22:41:31 -0300

    Changed ports

--- stephen/discuss/config/dev.exs
+++ stephen/discuss/config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :discuss, Discuss.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "discuss_dev",
+  username: System.get_env("PG_USERNAME", "postgres"),
+  password: System.get_env("PG_PASSWORD", "postgres"),
+  database: System.get_env("PG_DBNAME", "threefingers_dev"),
+  hostname: System.get_env("PG_HOST", "localhost"),
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -19,10 +19,8 @@ config :discuss, Discuss.Repo,
 config :discuss, DiscussWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
-  check_origin: false,
+  http: [port: 4200],
   code_reloader: true,
-  debug_errors: true,
   secret_key_base: "hbN2EwAJD27iz1aJ/o4Y0M9NSgGJQgB93Vyq1LSeW81IQckCU/PJsM9RVh5pV3sq",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
