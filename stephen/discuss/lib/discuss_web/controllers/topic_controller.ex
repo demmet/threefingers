676a99d456fd891746e73671fdcf4fe7b6cb1a74
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 16:29:10 -0300

    Added initial topic form

--- stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -5,7 +5,7 @@ defmodule DiscussWeb.TopicController do
 
   def new(conn, params) do
     changeset = Topic.changeset(%Topic{}, %{})
-    render(conn, "new.html")
+    render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, params) do
