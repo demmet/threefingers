5cb54c81bd5ec63253586b1fb7e7aff5378c5f03
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 19:07:25 -0300

    Fixed version related errors

--- stephen/discuss/mix.exs
+++ stephen/discuss/mix.exs
@@ -7,7 +7,7 @@ defmodule Discuss.MixProject do
       version: "0.1.0",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
-      compilers: [:gettext] ++ Mix.compilers(),
+      compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
@@ -50,7 +50,7 @@ defmodule Discuss.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:ueberauth, "~> 0.7"},
-      {:ueberauth_github, "~> 0.7"}
+      {:ueberauth_github, "~> 0.8.1"}
     ]
   end
 
