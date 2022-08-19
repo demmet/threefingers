0b50448a63d0ab4817a5136c1c08576f1760c7e7
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 19:55:31 -0300

    Added signin function to AuthController

--- stephen/discuss/lib/discuss_web/controllers/auth_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -4,12 +4,28 @@ defmodule DiscussWeb.AuthController do
 
   alias Discuss.Auth
 
-  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+  def callback(conn, params) do
+    signin(conn)
+  end
+
+  defp signin(%{assigns: %{ueberauth_auth: auth}} = conn) do
     %{
       email: auth.info.email,
       token: auth.credentials.token,
       provider: "github"
     }
     |> Auth.upsert_user()
+    |> case do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "Welcome back!")
+        |> put_session(:user_id, user.id)
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "Error signing in")
+        |> redirect(to: Routes.topic_path(conn, :index))
+    end
   end
 end
