d02b3f9772c8e9d060c1d053514d734a251c5cbe
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 17:48:02 -0300

    Added topic to edit form in update action

--- stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -31,8 +31,9 @@ defmodule DiscussWeb.TopicController do
   end
 
   def update(conn, %{"id" => topic_id, "topic" => topic_params}) do
-    topic_id
-    |> Discuss.get_topic()
+    topic = Discuss.get_topic(topic_id)
+
+    topic
     |> Discuss.update_topic(topic_params)
     |> case do
       {:ok, _topic} ->
@@ -41,7 +42,7 @@ defmodule DiscussWeb.TopicController do
         |> redirect(to: Routes.topic_path(conn, :index))
 
       {:error, changeset} ->
-        render(conn, "edit.html", changeset: changeset)
+        render(conn, "edit.html", changeset: changeset, topic: topic)
     end
   end
 
