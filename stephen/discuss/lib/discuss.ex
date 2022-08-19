08a0f7036db5ea176da2081688a74f19b1877ba3
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 16:58:40 -0300

    Moved create topic features to Discuss context

--- stephen/discuss/lib/discuss.ex
+++ stephen/discuss/lib/discuss.ex
@@ -6,4 +6,16 @@ defmodule Discuss do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+
+  alias Discuss.Topic
+  alias Discuss.Repo
+
+  @doc """
+  Creates a topic.
+  """
+  def create_topic(attrs \\ %{}) do
+    %Topic{}
+    |> Topic.changeset(attrs)
+    |> Repo.insert()
+  end
 end
--- stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,24 +2,18 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
-  alias Discuss.Repo
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(conn, %{"topic" => topic}) do
-    %Topic{}
-    |> Topic.changeset(topic)
-    |> Repo.insert
+  def create(conn, %{"topic" => topic_params}) do
+    Discuss.create_topic(topic_params)
     |> case do
       {:ok, topic} -> IO.inspect(topic)
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
-    # changeset = Topic.changeset(%Tipoc{}, topic)
-
-    # Repo.insert(changeset)
   end
 end
