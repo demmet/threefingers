c416595cdfb7b8f2d28d3a8fbff8043d9088db40
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 17:38:10 -0300

    Added edit and update actions to TopicController

--- stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -22,4 +22,26 @@ defmodule DiscussWeb.TopicController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+  def edit(conn, %{"id" => topic_id}) do
+    topic = Discuss.get_topic(topic_id)
+    changeset = Discuss.change_topic(topic)
+
+    render(conn, "edit.html", changeset: changeset, topic: topic)
+  end
+
+  def update(conn, %{"id" => topic_id, "topic" => topic_params}) do
+    topic_id
+    |> Discuss.get_topic()
+    |> Discuss.update_topic(topic_params)
+    |> case do
+      {:ok, topic} ->
+        conn
+        |> put_flash(:info, "Topic Updated")
+        |> redirect(to: Routes.topic_path(conn, :index))
+
+      {:error, changeset} ->
+        render(conn, "edit.html", changeset: changeset)
+    end
+  end
 end
