254f486d832c320496c27755809aeeb18ce0fa9e
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 16:47:18 -0300

    Added initial Topic insertion to create action

--- stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -2,13 +2,23 @@ defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
 
   alias Discuss.Topic
+  alias Discuss.Repo
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
     render(conn, "new.html", changeset: changeset)
   end
 
-  def create(_conn, %{"topic" => _topic}) do
+  def create(conn, %{"topic" => topic}) do
+    %Topic{}
+    |> Topic.changeset(topic)
+    |> Repo.insert
+    |> case do
+      {:ok, topic} -> IO.inspect(topic)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
+    # changeset = Topic.changeset(%Tipoc{}, topic)
 
+    # Repo.insert(changeset)
   end
 end
