4a059ca8b622248d559a569894fd2cbce5a585bf
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-19 19:39:10 -0300

    Added user creation to AuthController callback

--- stephen/discuss/lib/discuss_web/controllers/auth_controller.ex
+++ stephen/discuss/lib/discuss_web/controllers/auth_controller.ex
@@ -2,7 +2,14 @@ defmodule DiscussWeb.AuthController do
   use DiscussWeb, :controller
   plug Ueberauth
 
-  def callback(conn, params) do
+  alias Discuss.Auth
 
+  def callback(%{assigns: %{ueberauth_auth: auth}} = conn, params) do
+    %{
+      email: auth.info.email,
+      token: auth.credentials.token,
+      provider: "github"
+    }
+    |> Auth.upsert_user()
   end
 end
