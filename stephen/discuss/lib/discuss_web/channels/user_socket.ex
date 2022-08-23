23edeb9b8ba6329fedc741dd8bca5da025df525a
Demétrius Mânica <demeteixeira@hotmail.com>
2022-08-23 14:49:12 -0300

    Created CommentsChannel

--- stephen/discuss/lib/discuss_web/channels/user_socket.ex
+++ stephen/discuss/lib/discuss_web/channels/user_socket.ex
@@ -1,6 +1,11 @@
 defmodule DiscussWeb.UserSocket do
   use Phoenix.Socket
 
+  channel "comments:*", DiscussWeb.CommentsChannel
+  # get "/comments/:id", CommentController, :join, :handle_in
+
+  transport, :websocket, Phoenix.Transports.Websocket
+
   @impl true
   def connect(_params, socket, _connect_info) do
     {:ok, socket}
